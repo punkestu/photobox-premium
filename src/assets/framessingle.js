7daa60d34a39c9dbc67aa88e675dac3bd7a1ed77
punkestu <pangestubima89@gmail.com>
2026-07-31 20:09:27 +0700

    Refactor code structure for improved readability and maintainability

--- src/assets/framessingle.js
+++ src/assets/framessingle.js
@@ -1,4 +1,5 @@
 import Calendar from "./framessingle/calendar.png";
+import Receipt from "./framessingle/receipt.png";
 
 export function all() {
     return [
@@ -15,6 +16,19 @@ export function all() {
             width: 685,
             height: 1181,
             ratio: [640, 480],
+        }, {
+            key: "receipt",
+            framecount: 2,
+            display: Receipt,
+            direction: "vertical",
+            offsetleft: 61,
+            offsettop: 283,
+            offsetright: 61,
+            offsetbottom: 65,
+            gap: 12,
+            width: 685,
+            height: 1299,
+            ratio: [564, 408],
         }
     ];
 }
